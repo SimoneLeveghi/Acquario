7dec49fe21ea7aa8f3a473847bd1a07b36d31f10
simone.leveghi <simone.leveghi@buonarroti.tn.it>
2023-10-09 10:20:13 +0200

    feat: added constructor for Acquario

--- src/Acquario.java
+++ src/Acquario.java
@@ -1,3 +1,15 @@
 public class Acquario {
+    private Pesce[] pesci;
+    private int id;
+    private static int maxId;
 
+
+    public Acquario(Pesce[] pesci) {
+        this.pesci = new Pesce[pesci.length];
+        for(int i = 0; i < pesci.length; ++i) {
+            this.pesci[i] = pesci[i].clone();
+        }
+
+        this.id = maxId++;
+    }
 }
