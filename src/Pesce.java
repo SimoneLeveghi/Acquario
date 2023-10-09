a897928120d374fe9bb98e077f7e901096d40374
simone.leveghi <simone.leveghi@buonarroti.tn.it>
2023-10-09 10:15:33 +0200

    feat: added constructor for Pesce

--- src/Pesce.java
+++ src/Pesce.java
@@ -1,4 +1,10 @@
 public class Pesce {
+    private String specie;
+    private int id;
+    private static int maxId;
 
-
+    public Pesce(String specie) {
+        this.specie = specie;
+        this.id = maxId++;
+    }
 }
