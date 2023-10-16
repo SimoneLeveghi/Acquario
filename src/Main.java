3845aaf2c95d235cf6938d8bc03a8cc279139a0a
simone.leveghi <simone.leveghi@buonarroti.tn.it>
2023-10-16 08:06:03 +0200

    Redone method main for Main

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(
-                new Acquario(
-                        new Pesce[]{
-                                new Pesce("Prova"),
-                                new Pesce("Prova2")
-                        }
-                        ).toString()
-        );
+        Acquario a = new Acquario(new Pesce[]{
+                new Pesce("Prova"),
+                new Pesce("Prova2")
+        });
+
+        System.out.println(a.toString());
+
+        System.out.println(a.removePesce(1).toString());
+        a.addPesce(new Pesce("Prova3"));
+
+        System.out.println(a.toString());
+
+        Acquario a2 = new Acquario(a);
+        System.out.println(a2.toString());
     }
 }
